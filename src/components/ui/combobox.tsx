9d1e96badb35ccfb3165b0aae5f105549769c2cf
Tharvesh Muhaideen A <tharvesh2026@gmail.com>
2025-11-10 12:47:44 +0000

    In my application, the search select options  function only works with t

--- src/components/ui/combobox.tsx
+++ src/components/ui/combobox.tsx
@@ -72,9 +72,9 @@ export function Combobox({
               {options.map((option) => (
                 <CommandItem
                   key={option.value}
-                  value={option.label}
-                  onSelect={() => {
-                    onValueChange(option.value === value ? "" : option.value)
+                  value={option.value}
+                  onSelect={(currentValue) => {
+                    onValueChange(currentValue === value ? "" : currentValue)
                     setOpen(false)
                   }}
                 >
