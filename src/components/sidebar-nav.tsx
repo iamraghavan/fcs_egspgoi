129b071353b6be178496da5319dcf9ccdfb1a016
Tharvesh Muhaideen A <tharvesh2026@gmail.com>
2026-02-23 16:52:05 +0000

    add this positive remarks into the oa dashboard also

--- src/components/sidebar-nav.tsx
+++ src/components/sidebar-nav.tsx
@@ -45,6 +45,7 @@ const getAdminNav = (uid: string) => [
 ];
 
 const getOANav = (uid: string) => [
+  { name: "Issue Positive Credit", href: `/u/portal/dashboard/oa/positive?uid=${uid}`, icon: Award },
   { name: "Issue Remark", href: `/u/portal/dashboard/oa?uid=${uid}`, icon: MessageSquareWarning },
   { name: "Issued History", href: `/u/portal/dashboard/oa/history?uid=${uid}`, icon: History },
 ];
