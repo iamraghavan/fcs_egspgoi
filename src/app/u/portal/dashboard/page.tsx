60259ec172b8e5db725878a66cbe01bed9abd267
Tharvesh Muhaideen A <tharvesh2026@gmail.com>
2026-04-24 06:06:09 +0000

    Runtime ReferenceError
    AlertDialogTrigger is not defined
    
    src/app/u/po

--- src/app/u/portal/dashboard/page.tsx
+++ src/app/u/portal/dashboard/page.tsx
@@ -35,6 +35,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import { format, startOfMonth, parseISO, isValid } from "date-fns";
 import _ from "lodash";
